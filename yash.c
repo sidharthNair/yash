a2bdf7f64642c055b853c3f87df07a444e08a498
Sidharth Nair <sidnair@utexas.edu>
2021-08-27 14:06:42 -0500

    Added parsing for special identifiers

--- yash.c
+++ yash.c
@@ -3,13 +3,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
 
-void tokenize(char* input, char** tokens) {
+char* special_identifiers[6] = {NULL, ">", "<", "2>", "|", "&"};
+
+int tokenize(char* input, char** tokens) {
+    int length = 0;
     *tokens = strtok(input, " ");
     while (*tokens != NULL) {
         tokens++;
         *tokens = strtok(NULL, " ");
+        length++;
     }
+    return length;
 }
 
 int main(void) {
@@ -18,14 +24,29 @@ int main(void) {
         char input[2000];
         fgets(input, 2000, stdin); // get input from user
         input[strcspn(input, "\n")] = '\0'; // remove trailing newline
-        char** argv;
-        tokenize(input, argv);
+        char* argv[64];
+        int length = tokenize(input, argv);
+        int count = 0;
+        int special[64] = {0};
+        for (int i = 0; i < length; i++) {
+            for (int j = 1; j < 6; j++) {
+                if (!strcmp(argv[i], special_identifiers[j])) {
+                    count++;
+                    special[i] = j;
+                    break;
+                }
+            }
+            //printf("%d", special[i]);
+        }
+        //printf("\n%d\n", count);
         int pid = fork();
         if (pid == 0) {
             execvp(*argv, argv);
         }
         else {
-            wait(NULL);
+            signal(SIGINT, SIG_IGN);
+            wait((int *)NULL);
+            signal(SIGINT, SIG_DFL);
         }
     }
     return 0;
