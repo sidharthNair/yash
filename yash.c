727fc448452b94784213ff253e785727e024171a
Sidharth Nair <sidnair@utexas.edu>
2021-09-09 12:29:36 -0500

    file redirection

--- yash.c
+++ yash.c
@@ -4,6 +4,9 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int tokenize(char *input, char **tokens)
 {
@@ -18,11 +21,40 @@ int tokenize(char *input, char **tokens)
     return length;
 }
 
-void execute(char **argv)
+void redirect(char **argv) {
+    char* token = *argv;
+    while (token != NULL) {
+        if (!strcmp(token, "<")) {
+            *(argv++) = NULL;
+            int new_stdin = open(*(argv++), O_RDONLY|O_CREAT, S_IRWXU);
+            dup2(new_stdin, 0);
+            close(new_stdin);
+        }
+        else if (!strcmp(token, ">")) {
+            *(argv++) = NULL;
+            int new_stdout = open(*(argv++), O_WRONLY|O_CREAT, S_IRWXU);
+            dup2(new_stdout, 1);
+            close(new_stdout);
+        }
+        else if (!strcmp(token, "2>")) {
+            *(argv++) = NULL;
+            int new_stderr = open(*(argv++), O_WRONLY|O_CREAT, S_IRWXU);
+            dup2(new_stderr, 2);
+            close(new_stderr);
+        }
+        else {
+            argv++;
+        }
+        token = *argv;
+    }
+}
+
+void execute_reg(char **argv)
 {
     int pid = fork();
     if (pid == 0)
     {
+        redirect(argv);
         execvp(*argv, argv);
         exit(0);
     }
@@ -39,6 +71,7 @@ void execute_pipe(char **argv, int pipe_index)
     int child1 = fork();
     if (child1 == 0)
     {
+        redirect(argv);
         dup2(pipefd[1], 1);
         close(pipefd[0]);
         execvp(*argv, argv);
@@ -46,6 +79,7 @@ void execute_pipe(char **argv, int pipe_index)
     int child2 = fork();
     if (child2 == 0)
     {
+        redirect(argv + (pipe_index + 1));
         dup2(pipefd[0], 0);
         close(pipefd[1]);
         execvp(*(argv + (pipe_index + 1)), (argv + (pipe_index + 1)));
@@ -83,7 +117,7 @@ int main(void)
             execute_pipe(argv, pipe_index);
         }
         else {
-            execute(argv);
+            execute_reg(argv);
         }
     }
     return 0;
