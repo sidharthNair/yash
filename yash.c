8b5250059877e41515d482198e3ec1b21541b175
Sidharth Nair <sidnair@utexas.edu>
2021-09-09 11:39:00 -0500

    Reorganized, pipe commands working

--- yash.c
+++ yash.c
@@ -5,8 +5,6 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-char *special_identifiers[6] = {NULL, ">", "<", "2>", "|", "&"};
-
 int tokenize(char *input, char **tokens)
 {
     int length = 0;
@@ -20,77 +18,72 @@ int tokenize(char *input, char **tokens)
     return length;
 }
 
+void execute(char **argv)
+{
+    int pid = fork();
+    if (pid == 0)
+    {
+        execvp(*argv, argv);
+        exit(0);
+    }
+    else
+    {
+        wait((int *)NULL);
+    }
+}
+
+void execute_pipe(char **argv, int pipe_index)
+{
+    int pipefd[2];
+    pipe(pipefd);
+    int child1 = fork();
+    if (child1 == 0)
+    {
+        dup2(pipefd[1], 1);
+        close(pipefd[0]);
+        execvp(*argv, argv);
+    }
+    int child2 = fork();
+    if (child2 == 0)
+    {
+        dup2(pipefd[0], 0);
+        close(pipefd[1]);
+        execvp(*(argv + (pipe_index + 1)), (argv + (pipe_index + 1)));
+    }
+    else
+    {
+        close(pipefd[0]);
+        close(pipefd[1]);
+        waitpid(child1, NULL, 0);
+        waitpid(child2, NULL, 0);
+    }
+}
+
 int main(void)
 {
+    pid_t s_pid = getpid();
     while (1)
     {
-        printf("# ");
+        write(1, "# ", 2);
         char input[2048];
-        fgets(input, 2048, stdin);          // get input from user
-        input[strcspn(input, "\n")] = '\0'; // remove trailing newline
         char *argv[64];
+        fgets(input, 2048, stdin);
+        input[strcspn(input, "\n")] = '\0'; 
         int length = tokenize(input, argv);
-        int count = 0;
-        int special[64] = {0};
-        int pipe_flag = 0;
+        argv[length] = NULL;
         int pipe_index = 0;
         for (int i = 0; i < length; i++)
         {
-            for (int j = 1; j < 6; j++)
-            {
-                if (!strcmp(argv[i], special_identifiers[j]))
-                {
-                    count++;
-                    special[i] = j;
-                    if (j == 4)
-                        pipe_flag = 1;
-                        pipe_index = i;
-                        argv[i] = '\0';
-                    break;
-                }
-            }
-            //printf("%d", special[i]);
-        }
-        //printf("\n%d\n", count);
-        int pipefd[2];
-        for (int i = 0; i < length; i++) {
-            printf("%s ", argv[i]);
-        }
-        printf("\n%d %d\n", pipe_flag, pipe_index);
-        if (pipe_flag)
-        {
-            pipe(pipefd);
-            int ch1 = fork();
-            if (ch1 == 0) {
-                close(pipefd[0]);
-                dup2(pipefd[1],1);
-                printf("left: %s\n" , *argv);
-                execvp(*argv, argv);
-            }
-            int ch2 = fork();
-            if (ch2 == 0) {
-                close(pipefd[1]);
-                dup2(pipefd[0],0);
-                printf("right: %s\n" , *(argv + (pipe_index+1)));
-                execvp(*(argv + (pipe_index+1)), (argv + (pipe_index+1)));
-            }
-            else {
-                wait((int *)NULL);
+            if (!strcmp(argv[i], "|")) {
+                argv[i] = NULL;
+                pipe_index = i;
             }
         }
-        else
-        {
-            int pid = fork();
-            if (pid == 0)
-            {
-                execvp(*argv, argv);
-            }
-            else
-            {
-                signal(SIGINT, SIG_IGN);
-                wait((int *)NULL);
-                signal(SIGINT, SIG_DFL);
-            }
+        if (pipe_index) {
+            execute_pipe(argv, pipe_index);
+        }
+        else {
+            execute(argv);
         }
     }
     return 0;
